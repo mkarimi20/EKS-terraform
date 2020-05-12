6a2c68af0022d4ffd72621d20d81c4c7208df219
Mohammad Karimi <mohammadkarimi@MacBook-Pro.local>
2020-05-11 19:24:07 -0500

    eks terraform code added

---
+++ eks.tf
@@ -0,0 +1,30 @@
+data "aws_eks_cluster" "cluster" {
+  name = module.my-cluster.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.my-cluster.cluster_id
+}
+
+provider "kubernetes" {
+  host = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token = data.aws_eks_cluster_auth.cluster.token
+  load_config_file = false
+  version = "~> 1.9"
+}
+
+module "my-cluster" {
+  source = "terraform-aws-modules/eks/aws"
+  cluster_name = "my-cluster"
+  cluster_version = "1.14"
+  subnets = ["subnet-3a6c2f04",
+             "subnet-50350f7e",
+             "subnet-5d976653"]
+  vpc_id = "vpc-2ada9250"
+  worker_groups = [{
+    instance_type = "t2.mediom"
+    asg_max_size = 5
+    }
+  ]
+}
