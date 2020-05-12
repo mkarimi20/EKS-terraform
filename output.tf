1ad1bfe35a7c357cbd13f54d93d5073b78ac655a
Mohammad Karimi <mohammadkarimi@MacBook-Pro.local>
2020-05-11 19:59:43 -0500

    error

--- output.tf
+++ output.tf
@@ -1,17 +1,17 @@
 output "arn" {
-  value = "module.my_cluster.cluster.arn"
+  value = module.my_cluster.cluster.arn
 }
 
 output "cluster_id" {
-  value = "module.my_cluster.cluster.id"
+  value = module.my_cluster.cluster.id
 }
 
 output "Sec_group" {
-  value = "module.my_cluster.security_group.id"
+  value = module.my_cluster.security_group.id
 }
 
 output "region" {
-  value = "aws_provider.region"
+  value = aws_provider.region
 }
 
 
