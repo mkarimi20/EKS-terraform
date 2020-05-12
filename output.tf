ef31e6b1df4c96a4e68a86c2ddd85738fdc3da7b
Mohammad Karimi <mohammadkarimi@MacBook-Pro.local>
2020-05-11 20:56:29 -0500

    output updated

--- output.tf
+++ output.tf
@@ -2,12 +2,19 @@ output "arn" {
   value = module.my-cluster.cluster_arn
 }
 
-output "cluster_name" {
+output "cluster_ID" {
   value = module.my-cluster.cluster_id
 }
 
 output "Sec_group" {
   value = module.my-cluster.cluster_security_group_id
+}
+
+output "region" {
+  value = var.region
+}
+output "name" {
+  value = var.cluster_name
 }
 
 
